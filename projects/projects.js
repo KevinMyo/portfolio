f73ccde66ccd0dabb95e71dab37f5c25d60ae882
KevinMyo <amyo@ucsd.edu>
2025-01-31 20:09:31 -0800

    revert

--- projects/projects.js
+++ projects/projects.js
@@ -5,25 +5,27 @@ import { fetchJSON, renderProjects } from '../global.js';
       const projects = await fetchJSON('../lib/projects.json');
   
       const projectsContainer = document.querySelector('.projects');
-  
+      const projectsTitle = document.querySelector('.projects-title');
+
       if (!projectsContainer) {
         console.error("The container with class '.projects' was not found.");
         return;
       }
 
-      if (Array.isArray(projects) && projects.length > 0) {
-        // It’s important to clear the container only once before adding all project entries.
-        projectsContainer.innerHTML = '';
+      if (Array.isArray(projects) && projectsTitle) {
+        projectsTitle.textContent = `Projects (${projects.length})`;
+      }
 
+      projectsContainer.innerHTML = '';
+  
+      if (Array.isArray(projects) && projects.length > 0) {
         projects.forEach(project => {
           renderProjects(project, projectsContainer, 'h2');
         });
       } else {
-
         projectsContainer.innerHTML = '<p>No projects available at the moment.</p>';
       }
     } catch (error) {
-
       console.error('Error fetching or rendering projects:', error);
     }
   })();
